721197726f6e07d95300a73529e03a9f4ac77b8f
Nguyễn Quang Huy <Huy2004.ptitIS@gmail.com>
2026-05-17 16:49:29 +0700

    Update mailer.js

--- QLATTT-LandingPage-BE-main/src/utils/mailer.js
+++ QLATTT-LandingPage-BE-main/src/utils/mailer.js
@@ -2,8 +2,21 @@ const SibApiV3Sdk = require('sib-api-v3-sdk');
 
 const client = SibApiV3Sdk.ApiClient.instance;
 
-// DEBUG - xóa sau khi fix xong
 console.log('BREVO_API_KEY:', process.env.BREVO_API_KEY ? process.env.BREVO_API_KEY.substring(0, 20) + '...' : 'UNDEFINED');
 console.log('SMTP_USER:', process.env.SMTP_USER);
 
 client.authentications['api-key'].apiKey = process.env.BREVO_API_KEY;
+
+const apiInstance = new SibApiV3Sdk.TransactionalEmailsApi();
+
+const sendMail = async ({ to, toName, subject, html }) => {
+  const email = new SibApiV3Sdk.SendSmtpEmail();
+  email.sender = { name: 'ECO Riverside', email: process.env.SMTP_USER };
+  email.to = [{ email: to, name: toName || to }];
+  email.subject = subject;
+  email.htmlContent = html;
+
+  return apiInstance.sendTransacEmail(email);
+};
+
+module.exports = sendMail;
